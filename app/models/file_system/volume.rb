f0be80162c20f2e8e8669daf5dad3488ef22bde9
Rahoul Baruah <rahoulb@echodek.co>
2025-03-19 17:40:49 +0000

    Ensure volumes know about their folders

--- app/models/file_system/volume.rb
+++ app/models/file_system/volume.rb
@@ -3,10 +3,12 @@ module FileSystem
     attribute :name, :string
     normalizes :name, with: ->(name) { name.to_s.strip }
     validates :name, presence: true, uniqueness: true
-    has_many :items, class_name: "FileSystem::Item", dependent: :destroy
+    has_many :folders, -> { active.order :name }
 
     def to_s = name
 
     def to_param = "#{id}-#{name}".parameterize
+    has_many :items, class_name: "FileSystem::Item", dependent: :destroy
+    has_many :_folders, class_name: "FileSystem::Folder", dependent: :destroy
   end
 end
